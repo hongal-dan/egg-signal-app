7c20f3c9ebff413828f999ce4cb9ab6b1117de82
Inseok Kim <js2121good@gmail.com>
2024-06-24 01:49:59 +0900

    feat: 404페이지 뒤로가기 버튼 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
 
-type Props = {}
+import React from "react";
+import Image from "next/image";
 
-const NotFound = (props: Props) => {
+
+const NotFound = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <Image src="/img/404.png" alt="404-not-found" width={500} height={500}/>
-      <h1 className='text-[3rem]'>해당 페이지를 찾을 수 없습니다.</h1>
+    <div className="flex flex-col justify-center items-center">
+      <Image src="/img/404.png" alt="404-not-found" width={500} height={500} />
+      <h1 className="text-[3rem]">해당 페이지를 찾을 수 없습니다.</h1>
+      <button
+        className="border-2 p-2 border-black rounded-xl"
+        onClick={() => history.back()}
+      >
+        이전 페이지로
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
