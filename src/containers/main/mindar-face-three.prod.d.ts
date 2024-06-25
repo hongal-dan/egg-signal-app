067cae965f6d7b9f99a45c46655102af909b84b0
Inseok Kim <js2121good@gmail.com>
2024-06-25 22:47:45 +0900

    fix: import mindarthree

---
+++ src/containers/main/mindar-face-three.prod.d.ts
@@ -0,0 +1,15 @@
+declare module 'mind-ar/dist/mindar-face-three.prod.js' {
+  import * as THREE from 'three'; // Three.js 모듈 import 추가
+
+  export class MindARThree {
+    constructor(config: any);
+    start(): Promise<void>;
+    addAnchor(anchorIndex: number): any;
+    getLatestEstimate(): any;
+
+    // renderer, scene, camera 속성 추가 및 타입 정의
+    renderer: THREE.WebGLRenderer;
+    scene: THREE.Scene;
+    camera: THREE.Camera; 
+  }
+}
