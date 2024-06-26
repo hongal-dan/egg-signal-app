345d72f9dbb0db9f586e161e8d4acdf29d02733b
hyeong1 <mi75625@gmail.com>
2024-06-26 16:48:12 +0900

    add: recoil state 수정 가능 옵션

--- src/app/store/socket.ts
+++ src/app/store/socket.ts
@@ -1,9 +1,8 @@
-import {atom} from 'recoil';
-import { Socket } from 'socket.io-client';
+import { atom } from "recoil";
+import { Socket } from "socket.io-client";
 
 export const meetingSocketState = atom<Socket | null>({
-  key: 'meetingSocketState',
+  key: "meetingSocketState",
   default: null,
+  dangerouslyAllowMutability: true,
 });
-
-
