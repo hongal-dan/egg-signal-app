25cbf25c7055737d84bce57e1a6dcaeb053af4d9
Inseok Kim <js2121good@gmail.com>
2024-06-26 12:15:18 +0900

    fix: recoil mutable로 관리하도록 변경

--- src/app/meeting/[sessionId]/page.tsx
+++ src/app/meeting/[sessionId]/page.tsx
@@ -10,7 +10,7 @@ import {
   Device,
 } from "openvidu-browser";
 import { useRouter } from "next/navigation";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { meetingSocketState } from "@/app/store/socket";
 
 // type Props = {
@@ -33,17 +33,13 @@ const Meeting = () => {
     null,
   );
 
-  // const [isAvatar, setIsAvatar] = useState<boolean>(true);
   const [isLoveMode, setIsLoveMode] = useState<boolean>(false);
-  // const [isMatched, setIsMatched] = useState<boolean>(true);
   const [isChooseMode, setIsChooseMode] = useState<boolean>(false);
   const [isOneToOneMode, setIsOneToOneMode] = useState<boolean>(false);
-  // const videoRef = useRef<HTMLVideoElement>(null);
   const captureRef = useRef<HTMLDivElement>(null);
   const keywordRef = useRef<HTMLParagraphElement>(null);
 
-  // const url = process.env.NEXT_PUBLIC_API_SERVER;
-  const [socket] = useRecoilState(meetingSocketState);
+  const socket = useRecoilValue(meetingSocketState);
 
   // const socket = io(`${url}/meeting`, {
   //   transports: ["websocket"],
