9530abdaa811a0591b98871cc3103332d12b560f
hyeong1 <mi75625@gmail.com>
2024-06-29 21:51:51 +0900

    feat: 로그인 후 로그인한 유저 정보 받아오기

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,8 +1,10 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import MainContent from "@/containers/main/MainContent";
+import { getUserInfo } from "@/services/users";
 
-const Main = () => {
+const Main = async () => {
   const cookieStore = cookies();
   const token = cookieStore.get("access_token");
   const decodeJwt = (token: string) => {
@@ -12,17 +14,39 @@ const Main = () => {
     return payload;
   };
 
+  const handleGetUserInfo = async (token: RequestCookie) => {
+    try {
+      const response = await getUserInfo(token).then();
+      console.log(response.data);
+      const currentUser = {
+        id: response.data.id,
+        nickname: response.data.nickname,
+        gender: response.data.gender,
+        newNotification: response.data.newNotification,
+        notifications: response.data.notifications,
+        friends: response.data.friends,
+      };
+      return currentUser;
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
   if (token === undefined) {
     console.log("토큰 없음!");
     redirect("/login");
   }
+
+  const currentUser = await handleGetUserInfo(token);
+  console.log(currentUser);
+
   const userInfo = decodeJwt(token.value);
   console.log(userInfo);
 
   return (
     <div>
-      {token ? (
-        <MainContent nickname={userInfo.nickname} />
+      {token && currentUser ? (
+        <MainContent userInfo={currentUser} />
       ) : (
         <div>Redirecting...</div>
       )}
