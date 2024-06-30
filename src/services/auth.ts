40c36f8c812ac3371910c617f9b8bf8a859ffd14
Inseok Kim <js2121good@gmail.com>
2024-07-01 02:17:25 +0900

    feat: login next서버로 post

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -25,9 +25,10 @@ const createUser = async (request: CreateUser) => {
 
 const loginUser = async (request: LoginUser) => {
   try {
-    const response = await axios.post(`${authUrl}/signIn`, request, {
+    const response = await axios.post(`/api/auth/signIn`, request, {
       withCredentials: true,
     });
+    console.log("response = ", response);
     return response;
   } catch (error) {
     return error;
