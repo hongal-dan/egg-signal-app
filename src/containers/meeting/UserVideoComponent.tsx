1f5a241a54b15b86d9b46729c79d5f219580ef60
Inseok Kim <js2121good@gmail.com>
2024-07-01 02:53:50 +0900

    feat: 닉네임 파싱해서 보여주기
    Co-authored-by: hyeong1 <mi75625@gmail.com>
    
    Co-authored-by: hyeong1 <mi75625@gmail.com>

--- src/containers/meeting/UserVideoComponent.tsx
+++ src/containers/meeting/UserVideoComponent.tsx
@@ -1,23 +1,29 @@
-'use client';
-import React, { useRef, useEffect } from 'react';
-import OpenViduVideoComponent from './OvVideo';
+"use client";
+import React, { useRef, useEffect } from "react";
+import OpenViduVideoComponent from "./OvVideo";
+import { StreamManager } from "openvidu-browser";
 
-export default function UserVideoComponent(props: any) {
+// todo: 소켓 리코일에서 꺼내쓰기
+export default function UserVideoComponent(props: {
+  streamManager: StreamManager;
+  socket: any;
+}) {
   const streamComponentRef = useRef<HTMLDivElement>(null);
   const rawData = props.streamManager.stream.connection.data;
 
   // 데이터를 구분자로 분리
-  const [jsonString] = rawData.split('%/%');
+  const [jsonString] = rawData.split("%/%");
 
   // JSON 문자열을 파싱
-  let nickname = '';
+  let nickname = "";
   nickname = JSON.parse(jsonString).clientData;
+  // console.log(rawData)
 
   useEffect(() => {
     if (streamComponentRef.current) {
       streamComponentRef.current.id = nickname;
     }
-  });
+  }, []);
 
   return (
     <div>
@@ -28,7 +34,10 @@ export default function UserVideoComponent(props: any) {
               <div className="arrow-head"></div>
             </div>
           </div>
-          <OpenViduVideoComponent streamManager={props.streamManager} />
+          <OpenViduVideoComponent
+            streamManager={props.streamManager}
+            socket={props.socket}
+          />
           <div>
             <p className="nickname">{nickname}</p>
           </div>
