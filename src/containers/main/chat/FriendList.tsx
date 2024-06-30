673d1c2419e0fb68d862c17a8c1555e6442b1048
Inseok Kim <js2121good@gmail.com>
2024-07-01 02:50:42 +0900

    feat: 친구창 컴포넌트 생성
    Co-authored-by: hyeong1 <mi75625@gmail.com>
    
    Co-authored-by: hyeong1 <mi75625@gmail.com>

---
+++ src/containers/main/chat/FriendList.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Friend from "./Friend";
+import Chat from "./Chat";
+import { userState } from "@/app/store/userInfo";
+import { useRecoilValue } from "recoil";
+import { useCommonSocket } from "@/contexts/CommonSocketContext";
+
+interface FriendListProps {
+  onClose: () => void;
+  newMessageSenders: string[];
+}
+
+interface Friend {
+  friend: string;
+  chatRoomId: string;
+}
+
+const FriendList: React.FC<FriendListProps> = ({
+  newMessageSenders,
+  onClose,
+}) => {
+  const currentUser = useRecoilValue(userState);
+  const { commonSocket } = useCommonSocket();
+  const [selectedFriend, setSelectedFriend] = useState<Friend | null>(null);
+  const [isChatVisible, setIsChatVisible] = useState<boolean>(false);
+
+
+
+  useEffect(() => {
+    console.log(newMessageSenders);
+  });
+
+  return (
+    <div className="w-72 h-[700px] overflow-auto">
+      <div className="text-end">
+        <button onClick={onClose} className="font-bold">
+          ✕
+        </button>
+      </div>
+      {currentUser?.friends.map((friend, index) => (
+        <div key={index} className="relative">
+          {newMessageSenders &&
+            newMessageSenders.find(el => el === friend.chatRoomId) && (
+              <div className="absolute left-0 top-0 w-3 h-3 rounded-full bg-red-600" />
+            )}
+          <Friend friend={friend} onChat={() => toggleChat(friend)} />
+        </div>
+      ))}
+      {isChatVisible && selectedFriend && (
+        <div className="w-full absolute top-[250px] left-[-330px] bottom-0 bg-white shadow-md rounded-lg z-11">
+          <Chat friend={selectedFriend} onClose={closeChat} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FriendList;
