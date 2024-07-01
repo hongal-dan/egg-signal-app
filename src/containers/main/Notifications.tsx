ef33faba0eb6ef32dfd9d8b64678d056cba7a36a
hyeong1 <mi75625@gmail.com>
2024-07-02 02:19:16 +0900

    feat: 친구 요청 수락

--- src/containers/main/Notifications.tsx
+++ src/containers/main/Notifications.tsx
@@ -1,14 +1,39 @@
+"use client";
 import React from "react";
+import { userState } from "@/app/store/userInfo";
+import { commonSocketState } from "@/app/store/commonSocket";
+import { useRecoilValue } from "recoil";
 
-interface NotificationsProps {}
+interface NotificationsProps {
+  notiList: string[];
+}
 
-const Notification: React.FC = () => {
+interface Sender {
+  sender: string;
+}
+
+const Notification: React.FC<Sender> = ({ sender }) => {
+  const currentUser = useRecoilValue(userState);
+  const commonSocket = useRecoilValue(commonSocketState);
+
+  const acceptRequest = () => {
+    if (commonSocket && currentUser) {
+      commonSocket.emit("reqAcceptFriend", {
+        userNickname: currentUser.nickname,
+        friendNickname: sender,
+      });
+    }
+  };
+
   return (
     <div className="flex gap-[10px] border border-slate-500 bg-white rounded-2xl p-5">
       <div className="w-full flex justify-between">
-        <div className="text-center">이름</div>
+        <div className="text-center">{sender}</div>
         <div className="flex justify-center gap-2">
-          <button className="bg-green-500 text-white rounded-lg text-m px-3">
+          <button
+            className="bg-green-500 text-white rounded-lg text-m px-3"
+            onClick={acceptRequest}
+          >
             수락
           </button>
           <button className="bg-red-500 text-white rounded-lg  text-m px-3">
@@ -20,12 +45,14 @@ const Notification: React.FC = () => {
   );
 };
 
-const Notifications: React.FC<NotificationsProps> = () => {
+const Notifications: React.FC<NotificationsProps> = ({ notiList }) => {
   return (
     <div>
       <p>친구 요청</p>
       <div>
-        <Notification />
+        {notiList.map(sender => (
+          <Notification key={sender} sender={sender} />
+        ))}
       </div>
     </div>
   );
