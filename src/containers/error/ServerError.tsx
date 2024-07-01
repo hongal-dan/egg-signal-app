83518afddbb87af835b06b17867d1fec361a674a
Inseok Kim <js2121good@gmail.com>
2024-07-01 23:34:36 +0900

    update: 메인 서버 오류페이지 버튼 > 메인페이지로 가게끔

--- src/containers/error/ServerError.tsx
+++ src/containers/error/ServerError.tsx
@@ -2,19 +2,21 @@
 
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 type Props = {}
 
 const ServerError = (props: Props) => {
+  const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
       <h1 className="text-[3rem] relative top-5">서버 연결 실패</h1>
       <Image src="/img/500.png" alt="500-server-error" width={500} height={500} />
       <button
         className="border-2 p-2 border-black rounded-xl"
-        onClick={() => history.back()}
+        onClick={() => router.replace("/main")}
       >
-        이전 페이지로
+        메인 페이지로
       </button>
     </div>
   );
