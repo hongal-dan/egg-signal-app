61aa197e72896470963f23738c89f1c5a2f29f42
won-n-only <siliver12@gmail.com>
2024-07-02 02:41:03 +0900

    feat: add skeleton code for drawing Contest #28

---
+++ src/containers/meeting/CanvasMoadl.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React, { useRef, useEffect, useState } from "react";
+
+// import styles from "@/styles/canvas.css";
+
+type CanvasModalProps = {
+  onClose: () => void;
+};
+
+const CanvasModal: React.FC<CanvasModalProps> = ({ onClose }) => {
+  return;
+};
+
+export default CanvasModal;
