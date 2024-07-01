0c14c6ff98bcfe9808e7f35d92ebeb0cdf0cacc1
Inseok Kim <js2121good@gmail.com>
2024-07-01 21:28:08 +0900

    feat: 서버 연결 실패 컴포넌트

---
+++ src/containers/error/ServerError.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+type Props = {}
+
+const ServerError = (props: Props) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
+      <h1 className="text-[3rem] relative top-5">서버 연결 실패</h1>
+      <Image src="/img/500.png" alt="500-server-error" width={500} height={500} />
+      <button
+        className="border-2 p-2 border-black rounded-xl"
+        onClick={() => history.back()}
+      >
+        이전 페이지로
+      </button>
+    </div>
+  );
+}
+
+export default ServerError;
