964e0ecc9d4a9778a3ad6bdb19c65b6cd04ce815
hyeong1 <mi75625@gmail.com>
2024-07-02 01:48:02 +0900

    remove: Notification 프로필 이미지 삭제

--- src/containers/main/Notifications.tsx
+++ src/containers/main/Notifications.tsx
@@ -1,24 +1,13 @@
-import Image from "next/image";
 import React from "react";
 
-interface NotificationsProps {
-  onClose: () => void;
-}
+interface NotificationsProps {}
 
 const Notification: React.FC = () => {
   return (
-    // 테스트용 데이터
     <div className="flex gap-[10px] border border-slate-500 bg-white rounded-2xl p-5">
-      <Image
-        src="/img/profile_sample.png"
-        alt="profile"
-        width={70}
-        height={70}
-        className="rounded-xl"
-      />
-      <div className="w-full">
-        <p className="text-center">이름</p>
-        <div className="w-[100%] flex justify-center gap-2">
+      <div className="w-full flex justify-between">
+        <div className="text-center">이름</div>
+        <div className="flex justify-center gap-2">
           <button className="bg-green-500 text-white rounded-lg text-m px-3">
             수락
           </button>
