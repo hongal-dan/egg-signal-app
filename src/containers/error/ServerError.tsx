a523148c5926f6679fc810c242087a52440fc95e
Inseok Kim <js2121good@gmail.com>
2024-07-03 15:59:50 +0900

    fix: 메인 페이지 버튼 시 리프레쉬

--- src/containers/error/ServerError.tsx
+++ src/containers/error/ServerError.tsx
@@ -2,19 +2,19 @@
 
 import React from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 type Props = {}
 
 const ServerError = (props: Props) => {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
       <h1 className="text-[3rem] relative top-5">서버 연결 실패</h1>
       <Image src="/img/500.png" alt="500-server-error" width={500} height={500} />
       <button
         className="border-2 p-2 border-black rounded-xl"
-        onClick={() => router.replace("/main")}
+        onClick={() => window.location.reload()}
       >
         메인 페이지로
       </button>
