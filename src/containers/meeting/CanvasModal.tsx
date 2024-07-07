92c2e4bb412b890734fd97b54a0c3d912170c5c3
won-n-only <siliver12@gmail.com>
2024-07-07 21:26:29 +0900

    design: stage 'final'

--- src/containers/meeting/CanvasModal.tsx
+++ src/containers/meeting/CanvasModal.tsx
@@ -464,15 +464,38 @@ const CanvasModal: React.FC<CanvasModalProps> = ({ onClose }) => {
         )}
 
         {currentStage === "final" && finalResults && (
-          <>
+          <div className="flex flex-col items-center">
             <h2 className="text-xl font-bold mb-4">최종 결과</h2>
-            <div>좋은시간보내세요 {finalResults.winners.join(", ")}</div>
-            <div>
-              {finalResults.losers.length === Object.keys(drawings).length - 1
-                ? "안고를거면미팅왜했음"
-                : `나머지: ${finalResults.losers.join(", ")}`}
+            <div className="flex flex-col items-center">
+              <div className="flex gap-4 mb-4">
+                {finalResults.winners.map((winner, index) => (
+                  <div
+                    key={index}
+                    className="w-full relative border rounded-2xl p-1 bg-white shadow-md"
+                  >
+                    <img
+                      src={capturedPhoto[winner]}
+                      className="w-full h-full object-cover rounded-2xl"
+                    />
+                    <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-center py-1 rounded-b-2xl">
+                      {winner}
+                    </div>{" "}
+                    {index === 0 && (
+                      <div className="absolute top-0 left-0 p-2">
+                        <PiCrownSimpleDuotone className="text-yellow-500 text-2xl" />
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
-          </>
+
+            <div className="text-center">
+              {finalResults.losers.length === 5
+                ? "1등은 아무도 선택하지 않았습니다"
+                : "좋은 시간 되세요"}
+            </div>
+          </div>
         )}
       </div>
     </div>
