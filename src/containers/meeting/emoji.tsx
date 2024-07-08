e4046d61531d46f4dfa39d73b22be3d3afaa32bd
won-n-only <siliver12@gmail.com>
2024-07-08 21:03:28 +0900

    feat: func handleEmojiClick

--- src/containers/meeting/emoji.tsx
+++ src/containers/meeting/emoji.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 function Emoji() {
-  const handleEmojiClick = (emojiIndex: string) => {};
+  const socket = useRecoilValue(meetingSocketState);
+  const userInfo = useRecoilValue(userState);
+  const testName = useRecoilValue(testState); //FIXME 테스트용 랜덤 닉네임 저장, 배포 전에 삭제해야함
+
+  const handleEmojiClick = (emojiIndex: string) => {
+    console.log(testName, emojiIndex, "이모티콘 보냅니다");
+    socket?.emit("emoji", {
+      nickname: testName,
+      emojiIndex: emojiIndex,
+    });
+  };
 
   return <div></div>;
 }
