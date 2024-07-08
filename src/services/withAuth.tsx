01b84e9cc87b297cdbff4f61269a240f7f94791e
Inseok Kim <js2121good@gmail.com>
2024-07-08 22:54:02 +0900

    update: 리다이렉트 로딩 추가

--- src/services/withAuth.tsx
+++ src/services/withAuth.tsx
@@ -10,6 +10,7 @@ const withAuth = <Props extends object>(
   return (props: Props) => {  
     const router = useRouter();
     const pathname = usePathname();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
       const token = localStorage.getItem('token');
@@ -21,17 +22,29 @@ const withAuth = <Props extends object>(
         ) {
           router.push('/main'); 
         } else {
+          setIsLoading(false);
         }
       } else { // 사용자가 인증되지 않은 경우
         if (!isPublic) { 
           router.push('/login'); // 인증되지 않은 상태에서 접근하려면 '/login'으로 리다이렉트
         }
         else {
+          setIsLoading(false);
         }
       }
     }, [router, pathname]);
 
-
+    if (isLoading) {
+      return (
+        <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center gap-24">
+          <div className="flex flex-col items-center gap-4 text-3xl">
+            <p>로딩 중입니다.</p>
+            <p>잠시만 기다려주세요</p>
+          </div>
+          <span className="pan"></span>
+        </div>
+      );
+    }
 
     return <WrappedComponent {...props} />; 
   };
