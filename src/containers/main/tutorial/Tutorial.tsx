7a6d714cf9012e5de04c91812ca04229373df38f
hyeong1 <mi75625@gmail.com>
2024-07-08 21:10:15 +0900

    design: 튜토리얼 -> 가이드, ui 수정

--- src/containers/main/tutorial/Tutorial.tsx
+++ src/containers/main/tutorial/Tutorial.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import { GrFormPreviousLink, GrFormNextLink } from "react-icons/gr";
-import { IoRemoveOutline } from "react-icons/io5";
-import { GoChevronDown } from "react-icons/go";
 import Steps from "./Steps";
 
 const Tutorial = () => {
-  const [showTutorial, setShowTutorial] = useState(true);
+  const [showTutorial, setShowTutorial] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
 
   const handlePrev = () => {
@@ -16,16 +14,25 @@ const Tutorial = () => {
   const handleNext = () => {
     setCurrentStep(prevStep => (prevStep + 1) % Steps.length);
   };
+
+  const handleMouseEnter = () => {
+    setShowTutorial(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTutorial(false);
+  };
   return (
-    <div className="fixed top-10 left-10 w-[400px] bg-amber-100 rounded-3xl">
-      <p className="font-bold text-2xl text-center my-5">
-        에그팅 사용 방법
-        <button
-          className="absolute right-3 top-6"
-          onClick={() => setShowTutorial(prev => !prev)}
-        >
-          {showTutorial ? <IoRemoveOutline /> : <GoChevronDown />}
-        </button>
+    <div
+      className="fixed top-10 left-[-350px] w-[400px] bg-amber-100 rounded-3xl shadow-md transition-all duration-300 hover:left-5"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <p
+        className="text-ㅣㅎ text-end my-5 pr-1"
+        onClick={() => setShowTutorial(prev => !prev)}
+      >
+        가이드
       </p>
       <div className={`${!showTutorial && "hidden"}`}>
         <div className="h-[550px] p-5">{Steps[currentStep]}</div>
