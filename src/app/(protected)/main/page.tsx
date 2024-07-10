9a688b35d3c0be8eeaa119ed2d3d7688d5d3b8ab
Inseok Kim <js2121good@gmail.com>
2024-07-10 18:13:29 +0900

    remove: 서버에러 삭제

--- src/app/(protected)/main/page.tsx
+++ src/app/(protected)/main/page.tsx
@@ -1,37 +1,20 @@
 "use client";
 
 import MainContent from "@/containers/main/MainContent";
-import ServerError from "@/containers/error/ServerError";
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 const Main = () => {
   const [isMounted, setIsMounted] = useState(false);
-  const [isError, setIsError] = useState(true);
-
-  const checkServerHealth = async () => {
-    try {
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_SERVER}`);
-      if (response.status === 200) {
-        setIsError(false);
-      }
-    } catch (error) {
-      console.error("서버 연결 실패: ", error);
-      setIsError(true);
-    }
-  };
 
   useEffect(() => {
     setIsMounted(true);
-    checkServerHealth();
     return () => {
       setIsMounted(false);
-      setIsError(false);
     };
   }, []);
 
   return isMounted ? (
-    <>{isError ? <ServerError /> : <MainContent />}</>
+    <MainContent />
   ) : (
     <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center gap-24">
       <div className="flex flex-col items-center gap-4 text-3xl">
