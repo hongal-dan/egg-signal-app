cc5ca1327eff627c41141c4c945a985bff9c961f
hyeong1 <mi75625@gmail.com>
2024-07-13 16:32:46 +0900

    refactor: 친구 요청 버튼, 알림창 컴포넌트 분리

--- src/containers/main/MainContent.tsx
+++ src/containers/main/MainContent.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef, useState, useEffect } from "react";
 import FriendList from "./chat/FriendList";
-import Notifications from "./Notifications";
+import NotificationButton from "./button/NotificationsButton";
 import io from "socket.io-client";
 import { useRouter } from "next/navigation";
 import { useRecoilState } from "recoil";
@@ -59,7 +59,6 @@ const MainContent = () => {
 
   const [, setDefaultUserInfo] = useRecoilState(defaultSessionState);
   const [chatExpanded, setChatExpanded] = useState(false);
-  const notiRef = useRef<HTMLDivElement>(null);
 
   const checkOnlineFriends = () => {
     const onlineList = sessionStorage.getItem("onlineFriends");
@@ -333,21 +332,11 @@ const MainContent = () => {
         <div className="w-full flex justify-center items-center">
           <div className="flex flex-col items-center">
             <div className="flex justify-end w-full mb-2">
-              <div className="w-10 h-10 relative flex items-center justify-center text-xl bg-white rounded-2xl custom-shadow">
-                {notiList.length !== 0 && (
-                  <div className="absolute left-[-5px] top-[-5px] w-4 h-4 rounded-full bg-rose-500 custom-shadow" />
-                )}
-                <button onClick={toggleNotiList}>🔔</button>
-                {isNotiVisible && (
-                  <div
-                    ref={notiRef}
-                    onClick={e => e.stopPropagation()}
-                    className="w-[340px] h-[500px] absolute top-0 left-[50px] bg-zinc-200 shadow-md rounded-lg p-4 z-10"
-                  >
-                    <Notifications />
-                  </div>
-                )}
-              </div>
+              <NotificationButton
+                isNotiVisible={isNotiVisible}
+                notiList={notiList}
+                toggleNotiList={toggleNotiList}
+              />
             </div>
             <WebcamDisplay />
             <div className="w-full mt-4 relative">
