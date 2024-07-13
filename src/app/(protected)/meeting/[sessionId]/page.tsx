1df09cc56c72ada6c1ca1b029be7afe94c677767
hyeong1 <mi75625@gmail.com>
2024-07-13 22:35:31 +0900

    refactor: 짝대기 헬퍼 함수 분리

--- src/app/(protected)/meeting/[sessionId]/page.tsx
+++ src/app/(protected)/meeting/[sessionId]/page.tsx
@@ -32,6 +32,7 @@ import "animate.css";
 import Emoji from "@/containers/meeting/emoji";
 import { createRoot } from "react-dom/client";
 import Swal from "sweetalert2";
+import { showArrow, hideArrow } from "@/utils/meetingUtils";
 
 type chooseResult = {
   sender: string;
@@ -330,108 +331,108 @@ const Meeting = () => {
     }
   };
 
-  // 화살표 출발 도착 좌표 계산
-  const findPosition = (
-    fromElement: HTMLDivElement,
-    toElement: HTMLDivElement,
-  ): Array<number> => {
-    const rect1 = fromElement.getBoundingClientRect();
-    const rect2 = toElement.getBoundingClientRect();
-    let acc = 0;
-    if (fromElement.classList.contains("MALE")) {
-      acc = 10;
-    } else {
-      acc = -10;
-    }
+  // // 화살표 출발 도착 좌표 계산
+  // const findPosition = (
+  //   fromElement: HTMLDivElement,
+  //   toElement: HTMLDivElement,
+  // ): Array<number> => {
+  //   const rect1 = fromElement.getBoundingClientRect();
+  //   const rect2 = toElement.getBoundingClientRect();
+  //   let acc = 0;
+  //   if (fromElement.classList.contains("MALE")) {
+  //     acc = 10;
+  //   } else {
+  //     acc = -10;
+  //   }
 
-    if (
-      fromElement.classList.contains("a") ||
-      fromElement.classList.contains("b") ||
-      fromElement.classList.contains("c")
-    ) {
-      const startX1 = rect1.right;
-      const startY1 = rect1.top + rect1.height / 2;
-      const endX2 = rect2.left;
-      const endY2 = rect2.top + rect2.height / 2;
-      return [startX1, startY1 + acc, endX2, endY2 - acc];
-    } else {
-      const startX1 = rect1.left;
-      const startY1 = rect1.top + rect1.height / 2;
-      const endX2 = rect2.right;
-      const endY2 = rect2.top + rect2.height / 2;
-      return [startX1, startY1 + acc, endX2, endY2 - acc];
-    }
-  };
+  //   if (
+  //     fromElement.classList.contains("a") ||
+  //     fromElement.classList.contains("b") ||
+  //     fromElement.classList.contains("c")
+  //   ) {
+  //     const startX1 = rect1.right;
+  //     const startY1 = rect1.top + rect1.height / 2;
+  //     const endX2 = rect2.left;
+  //     const endY2 = rect2.top + rect2.height / 2;
+  //     return [startX1, startY1 + acc, endX2, endY2 - acc];
+  //   } else {
+  //     const startX1 = rect1.left;
+  //     const startY1 = rect1.top + rect1.height / 2;
+  //     const endX2 = rect2.right;
+  //     const endY2 = rect2.top + rect2.height / 2;
+  //     return [startX1, startY1 + acc, endX2, endY2 - acc];
+  //   }
+  // };
 
-  // 성별에 따라 화살표 색 변경
-  const setArrowColor = (
-    fromElement: HTMLDivElement,
-    arrow: Array<HTMLDivElement>,
-  ) => {
-    const [Head, Body] = arrow;
-    if (fromElement.classList.contains("MALE")) {
-      Head.style.borderBottom = "20px solid #33C4D7";
-      Body.style.backgroundColor = "#33C4D7";
-      return;
-    }
-    Head.style.borderBottom = "20px solid #fa3689";
-    Body.style.backgroundColor = "#fa3689";
-  };
+  // // 성별에 따라 화살표 색 변경
+  // const setArrowColor = (
+  //   fromElement: HTMLDivElement,
+  //   arrow: Array<HTMLDivElement>,
+  // ) => {
+  //   const [Head, Body] = arrow;
+  //   if (fromElement.classList.contains("MALE")) {
+  //     Head.style.borderBottom = "20px solid #33C4D7";
+  //     Body.style.backgroundColor = "#33C4D7";
+  //     return;
+  //   }
+  //   Head.style.borderBottom = "20px solid #fa3689";
+  //   Body.style.backgroundColor = "#fa3689";
+  // };
 
-  const showArrow = (datas: Array<chooseResult>) => {
-    datas.forEach(({ sender, receiver }) => {
-      const fromUser = document.getElementById(sender) as HTMLDivElement;
-      const toUser = document.getElementById(receiver) as HTMLDivElement;
-      const arrowContainer = fromUser?.querySelector(
-        ".arrow-container",
-      ) as HTMLDivElement;
-      const arrowBody = arrowContainer?.querySelector(
-        ".arrow-body",
-      ) as HTMLDivElement;
-      const arrowHead = arrowBody?.querySelector(
-        ".arrow-head",
-      ) as HTMLDivElement;
+  // const showArrow = (datas: Array<chooseResult>) => {
+  //   datas.forEach(({ sender, receiver }) => {
+  //     const fromUser = document.getElementById(sender) as HTMLDivElement;
+  //     const toUser = document.getElementById(receiver) as HTMLDivElement;
+  //     const arrowContainer = fromUser?.querySelector(
+  //       ".arrow-container",
+  //     ) as HTMLDivElement;
+  //     const arrowBody = arrowContainer?.querySelector(
+  //       ".arrow-body",
+  //     ) as HTMLDivElement;
+  //     const arrowHead = arrowBody?.querySelector(
+  //       ".arrow-head",
+  //     ) as HTMLDivElement;
 
-      const rect1 = fromUser.getBoundingClientRect();
-      const [startX1, startY1, endX2, endY2] = findPosition(fromUser, toUser);
+  //     const rect1 = fromUser.getBoundingClientRect();
+  //     const [startX1, startY1, endX2, endY2] = findPosition(fromUser, toUser);
 
-      const deltaX = endX2 - startX1;
-      const deltaY = endY2 - startY1;
-      const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-      setArrowColor(fromUser, [arrowHead, arrowBody]);
-      arrowContainer.style.paddingTop = "1rem";
-      // arrowBody.style.width = distance - 20 + "px";
-      arrowContainer.style.top = startY1 - rect1.top + "px";
-      arrowContainer.style.left = startX1 - rect1.left + "px";
+  //     const deltaX = endX2 - startX1;
+  //     const deltaY = endY2 - startY1;
+  //     const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+  //     setArrowColor(fromUser, [arrowHead, arrowBody]);
+  //     arrowContainer.style.paddingTop = "1rem";
+  //     // arrowBody.style.width = distance - 20 + "px";
+  //     arrowContainer.style.top = startY1 - rect1.top + "px";
+  //     arrowContainer.style.left = startX1 - rect1.left + "px";
 
-      arrowBody.style.setProperty('--arrow-width', `${distance - 20}px`);
+  //     arrowBody.style.setProperty('--arrow-width', `${distance - 20}px`);
 
-      arrowContainer.style.transform = `rotate(${
-        (Math.atan2(deltaY, deltaX) * 180) / Math.PI
-      }deg)`;
+  //     arrowContainer.style.transform = `rotate(${
+  //       (Math.atan2(deltaY, deltaX) * 180) / Math.PI
+  //     }deg)`;
 
-      if(fromUser.classList.contains("a") || fromUser.classList.contains("b") || fromUser.classList.contains("c")) {
-        arrowContainer.classList.remove("hidden");
-        arrowBody.style.animation = 'none';
-        arrowBody.offsetHeight;
-        arrowBody.style.animation = 'growArrow 2s ease-out forwards';
-        return;
-      }
-      setTimeout(() => {
-        arrowContainer.classList.remove("hidden");
-        arrowBody.style.animation = 'none';
-        arrowBody.offsetHeight;
-        arrowBody.style.animation = 'growArrow 2s ease-out forwards';
-      }, 3000);
-    });
-  };
+  //     if(fromUser.classList.contains("a") || fromUser.classList.contains("b") || fromUser.classList.contains("c")) {
+  //       arrowContainer.classList.remove("hidden");
+  //       arrowBody.style.animation = 'none';
+  //       arrowBody.offsetHeight;
+  //       arrowBody.style.animation = 'growArrow 2s ease-out forwards';
+  //       return;
+  //     }
+  //     setTimeout(() => {
+  //       arrowContainer.classList.remove("hidden");
+  //       arrowBody.style.animation = 'none';
+  //       arrowBody.offsetHeight;
+  //       arrowBody.style.animation = 'growArrow 2s ease-out forwards';
+  //     }, 3000);
+  //   });
+  // };
 
-  const hideArrow = () => {
-    const arrowContainers = document.querySelectorAll(".arrow-container");
-    arrowContainers.forEach(arrowContainer => {
-      arrowContainer.classList.add("hidden");
-    });
-  };
+  // const hideArrow = () => {
+  //   const arrowContainers = document.querySelectorAll(".arrow-container");
+  //   arrowContainers.forEach(arrowContainer => {
+  //     arrowContainer.classList.add("hidden");
+  //   });
+  // };
 
   const changeLoveStickMode = (datas: Array<chooseResult>) => {
     const videoElements = document.querySelectorAll("video");
@@ -622,14 +623,17 @@ const Meeting = () => {
     console.log("상대방: ", loverElement);
   };
 
-
   //FIXME 시연용 룰렛 함수
   const randomUser = (keywordIdx: number, pickUser: string) => {
     const streamElements = document.getElementsByClassName("stream-container");
     const streamArray = Array.from(streamElements);
     const tickSound = document.getElementById("tickSound") as HTMLAudioElement;
 
-    const rouletteElements = streamArray.slice(0, streamArray.length / 2).concat(streamArray.slice(streamArray.length / 2).reverse()) as HTMLDivElement[];
+    const rouletteElements = streamArray
+      .slice(0, streamArray.length / 2)
+      .concat(
+        streamArray.slice(streamArray.length / 2).reverse(),
+      ) as HTMLDivElement[];
 
     const totalIterations = 36; // 원하는 총 반복 횟수
     const minDuration = 10; // 초기 강조 시간 간격
@@ -656,7 +660,9 @@ const Meeting = () => {
         if (pubRef.current?.id === pickUser) {
           changePresentationMode(pubRef.current, 11, randomKeyword);
         } else {
-          const presenterElement = subRef.current?.filter(sub => sub?.id === pickUser)[0];
+          const presenterElement = subRef.current?.filter(
+            sub => sub?.id === pickUser,
+          )[0];
           if (presenterElement) {
             changePresentationMode(presenterElement, 11, randomKeyword);
           }
@@ -680,7 +686,8 @@ const Meeting = () => {
 
       // 비선형적으로 증가하는 시간 간격 계산 (제곱 함수 사용)
       const progress = iteration / totalIterations;
-      const duration = minDuration * Math.pow((maxDuration / minDuration), progress * 1.5);
+      const duration =
+        minDuration * Math.pow(maxDuration / minDuration, progress * 1.5);
       clearInterval(intervalId);
       intervalId = setInterval(highlightUser, duration);
     };
@@ -710,7 +717,7 @@ const Meeting = () => {
             if (sessionRef.current) {
               sessionRef.current.classList.remove("bg-black");
             }
-            if(keywordRef.current) {
+            if (keywordRef.current) {
               keywordRef.current.classList.remove("text-white");
             }
           }, 22000);
@@ -1235,14 +1242,14 @@ const Meeting = () => {
   }, [avatar]);
 
   useEffect(() => {
-    if(!isChosen) {
+    if (!isChosen) {
       return;
     }
-    if(chooseTimerRef.current) {
+    if (chooseTimerRef.current) {
       clearTimeout(chooseTimerRef.current);
       chooseTimerRef.current = null;
     }
-  }, [isChosen])
+  }, [isChosen]);
 
   const leaveHandler = () => {
     Swal.fire({
@@ -1307,7 +1314,11 @@ const Meeting = () => {
               ></audio>
             </div>
           </div>
-          <div id="session" className="h-full flex justify-center items-center transition-colors duration-[1500ms] ease-in-out" ref={sessionRef}>
+          <div
+            id="session"
+            className="h-full flex justify-center items-center transition-colors duration-[1500ms] ease-in-out"
+            ref={sessionRef}
+          >
             {/* <div ref={captureRef} className="hidden">
           <UserVideoComponent2 />
         </div> */}
