6b2c80cf859c0c29e7af3cfff275674ba5c7196b
hyeong1 <mi75625@gmail.com>
2024-07-14 17:23:43 +0900

    fix: 입장하기 버튼 누르면 disabled

--- src/containers/main/button/EnterButton.tsx
+++ src/containers/main/button/EnterButton.tsx
@@ -84,6 +84,7 @@ const EnterButton = () => {
       <button
         className="w-full h-12 bg-amber-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-1 z-10 relative custom-shadow"
         onClick={handleLoadingOn}
+        disabled={isLoading}
       >
         {isLoading ? (
           <svg
