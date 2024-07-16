46fdf86ea6eedf022b67533875fa23dd41e036ac
hyeong1 <mi75625@gmail.com>
2024-07-16 20:25:50 +0900

    chore: bundle-analyzer 추가, tree shaking 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -6,4 +12,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
