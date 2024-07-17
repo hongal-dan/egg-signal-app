8236c59253e94d5a0270e8e25a0a77c3771b0e43
Inseok Kim <js2121good@gmail.com>
2024-07-17 14:53:46 +0900

    refactor: 로딩 서스펜스 적용

--- src/app/(protected)/main/page.tsx
+++ src/app/(protected)/main/page.tsx
@@ -1,20 +1,14 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
 import MainContent from "@/containers/main/MainContent";
 import Loading from "@/containers/main/Loading";
 
 const Main = () => {
-  const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
-    return () => {
-      setIsMounted(false);
-    };
-  }, []);
-
-  return !isMounted ? <Loading /> : <MainContent />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <MainContent />
+    </Suspense>
+  );
 };
 
 export default Main;
